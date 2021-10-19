88c3c508bf368488a03cfdb2f9deab2cb808eda2
R-Morton <remann12@gmail.com>
2021-10-19 12:45:48 +1100

    Added functionality of generating random message

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-let toDo = [];
-let time = [];
-let why = [];
+let toDo = ['clean apartment', 'go to the gym', 'go shopping', 'meditate', 'socialise with friends'];
+let why = ['feel great', 'get it out of the way', 'the wife will stop nagging you', 'you can check it off your list', 'dont have to think about it anymore'];
+
+function randomiseArray(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+function giveRandomMinutes() {
+    return Math.floor(Math.random() * 181);
+
+}
+
+function generateList() {
+    let resultMessage = [];
+    resultMessage.push(`today you should ${toDo[randomiseArray(toDo)]}`);
+    resultMessage.push(`which will only take you ${giveRandomMinutes()} minutes`)
+    resultMessage.push(`so that ${why[randomiseArray(why)]}`);
+
+    return resultMessage.join(' ');
+}
+
+console.log(generateList());
